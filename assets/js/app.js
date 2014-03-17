571af9459f52e23c435c30c81214a6c308d14227
Zeph Lafassett <zeph@lafassett.com>
2014-03-17 07:42:53 -0700

    moving startup code into app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,30 @@
 var ContactManager = new Marionette.Application();
 
+ContactManager.StaticView = Marionette.ItemView.extend({
+	id: "static-view",
+	tagName: "span",
+	className: "instruction",
+	template: "#static-template"
+});
+
+
 // Regions and Templates
 ContactManager.addRegions({
 	mainRegion: "#main-region",
 	contactRegion: "#contact-region"
 });
+
+
+ContactManager.on("initialize:after", function () {
+
+	ContactManager.ContactsApp.List.Controller.listController();
+
+	var staticView = new ContactManager.StaticView({
+		template: "#different-static-template"
+	});
+
+	ContactManager.mainRegion.show(staticView);
+});
+
+
 
