01993a510283ad53d317d64d525406047db96514
Zeph Lafassett <zeph@lafassett.com>
2014-03-17 07:48:51 -0700

    converting list into table

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,12 +1,13 @@
 ContactManager.module('ContactsApp.List', function (List, ContactManager, Backbone, Marionette, $, _) {
 
 	List.Contact = Marionette.ItemView.extend({
-		tagName: "li",
+		tagName: "tr",
 		template: "#contact-list-item"
 	});
 
 	List.Contacts = Marionette.CollectionView.extend({
-		tagName: "ul",
+		tagName: "table",
+		className: "table table-hover",
 		itemView: List.Contact
 	});
 });
