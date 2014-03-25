cdd7bc46008cc6c45750582ec7a2cb93bcafdd7a
Zeph Lafassett <zeph@lafassett.com>
2014-03-25 09:00:59 -0700

    hooking up common view to edit form

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -1,6 +1,5 @@
 ContactManager.module("ContactsApp.Edit", function (Edit, ContactManager, Backbone, Marionette, $, _) {
-	Edit.Contact = Marionette.ItemView.extend({
-		template: "#contact-form",
+	Edit.Contact = ContactManager.ContactsApp.Common.Views.Form.extend({
 
 		initialize: function () {
 			this.title = "Edit " + this.model.get('firstName') + " " + this.model.get('lastName');
