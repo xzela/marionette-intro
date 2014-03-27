ab3405c7297649e56d7f2dea41657023efe7a566
Zeph Lafassett <zeph@lafassett.com>
2014-03-27 09:00:53 -0700

    Adding applyFilter fuction to common enities

--- assets/js/entities/common.js
+++ assets/js/entities/common.js
@@ -5,6 +5,36 @@ ContactManager.module('Entities', function (Entities, ContactManager, Backbone, 
 		filtered.add(original.models);
 		filtered.filterFunction = options.filterFunction;
 
+
+		var applyFilter = function (filterCriterion, filterStrategy) {
+			var collection = original;
+			var criterion;
+			if (filterStrategy === 'filter') {
+				criterion = filterCriterion.trim();
+			} else {
+				criterion = filterCriterion;
+			}
+
+			var items = [];
+			if (criterion) {
+				if (filterStrategy === 'filter') {
+					if (!filtered.filterFunction) {
+						throw ('no filter fuction found!');
+					}
+					var filterFunction = filtered.filterFunction(criterion);
+					items = collection.filter(filterFunction);
+				} else {
+					items = collection.where(criterion);
+				}
+			} else {
+				items = collection.models;
+			}
+			filtered._currentCriterion = criterion;
+
+			return items;
+		};
+
+
 		return filtered;
 	};
 });
